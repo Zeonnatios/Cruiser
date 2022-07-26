a0ff0d0ff4de03e8ba6b9cead193693ba260cf75
Matheus Antonio <matheusantonio232@gmail.com>
2022-07-26 16:42:06 -0300

    refactoring project using spring framework

---
+++ cruiser-app/src/main/java/com/cruiser/app/CruiserAppApplication.java
@@ -0,0 +1,13 @@
+package com.cruiser.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruiserAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruiserAppApplication.class, args);
+	}
+
+}
---
+++ cruiser-app/src/test/java/com/cruiser/app/CruiserAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cruiser.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruiserAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
