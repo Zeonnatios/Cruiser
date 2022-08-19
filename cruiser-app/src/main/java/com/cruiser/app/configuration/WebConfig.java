43b1eb121912558291f4743cb1c6b1c1fd96db76
Zeonnatios <matheusantonio232@gmail.com>
2022-08-18 23:07:54 -0300

    added login page

---
+++ cruiser-app/src/main/java/com/cruiser/app/configuration/WebConfig.java
@@ -0,0 +1,26 @@
+package com.cruiser.app.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = { "com.app.controller" })
+public class WebConfig implements WebMvcConfigurer {
+
+	  @Override
+	  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+	    registry
+	        .addResourceHandler("/static/**") // « /static/css/myStatic.css
+	        .addResourceLocations("classpath:/static/") // Default Static Loaction
+	        .setCachePeriod( 3600 )
+	        .resourceChain(true)
+	        .addResolver(new PathResourceResolver());
+
+	  }
+}
